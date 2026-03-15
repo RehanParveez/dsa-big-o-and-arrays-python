9f4c6e0735b9942a8378a60a50a2a401e24926cf
Rehan Parveez <rehanparveez@gmail.com>
2026-03-15 22:04:33 +0500

    this commit is about some practice

--- Topic 3, Linked Lists/problems/linked_list.py
+++ Topic 3, Linked Lists/problems/linked_list.py
@@ -53,28 +53,107 @@
   
   
 ## Q: Problem: Remove Nodes Smaller Than Next Node 
+##1
+# class Node:
+#   def __init__(self, data):
+#     self.data = data
+#     self.next = None
+
+# def remove(head):
+#   curr = head
+#   while curr and curr.next:
+#     if curr.data < head.data:
+#       curr = curr.next
+#     else:
+#       break
+#   return curr
+
+# head = Node(6)
+# head.next = Node(5)
+# head.next.next = Node(10)
+# head.next.next.next = Node(3)
+# head.next.next.next.next = Node(8)
+# res = remove(head)
+
+# while res:
+#   print(res.data)
+#   res = res.next
+  
+## 2
+# class Node:
+#   def __init__(self, data):
+#     self.data = data
+#     self.next = None
+    
+# def remove(head):
+#   curr = head
+#   prev = None
+
+#   while curr and curr.next:
+#     if curr.data < curr.next.data:
+#       if prev:
+#         prev.next = curr.next
+#       else:
+#         head = curr.next
+#       curr = curr.next
+#     else:
+#       prev = curr
+#       curr = curr.next
+#   return head
+
+# head = Node(5)
+# head.next = Node(3)
+# head.next.next = Node(10)
+# head.next.next.next = Node(2)
+# head.next.next.next.next = Node(8)
+
+# res = remove(head)
+# while res:
+#   print(res.data)
+#   res = res.next
+
+## 3
 class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
 
 def remove(head):
+  prev = None
+  curr = head
+  while curr:
+    nxt = curr.next
+    curr.next = prev
+    prev = curr
+    curr = nxt
+  head = prev  
+
+  max = head.data
   curr = head
   while curr and curr.next:
-    if curr.data < head.data:
-      curr = curr.next
+    if curr.next.data < max:
+      curr.next = curr.next.next
     else:
-      break
-  return curr
+      curr = curr.next
+      max = curr.data
 
-head = Node(6)
-head.next = Node(5)
+  prev = None
+  curr = head
+  while curr:
+    nxt = curr.next
+    curr.next = prev
+    prev = curr
+    curr = nxt
+  return prev
+
+head = Node(5)
+head.next = Node(3)
 head.next.next = Node(10)
-head.next.next.next = Node(3)
+head.next.next.next = Node(2)
 head.next.next.next.next = Node(8)
-res = remove(head)
 
+res = remove(head)
 while res:
-  print(res.data)
-  res = res.next
+    print(res.data)
+    res = res.next
     
