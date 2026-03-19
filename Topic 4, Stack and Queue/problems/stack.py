7864a651852085312555e38c7c277da51fbdd137
Rehan Parveez <rehanparveez@gmail.com>
2026-03-19 22:18:01 +0500

    thi commit is about prac of prob.

--- Topic 4, Stack and Queue/problems/stack.py
+++ Topic 4, Stack and Queue/problems/stack.py
@@ -157,6 +157,113 @@
 # print(greater([4, 3, 4, 6, 5])) 
 # print(greater([6, 5, 4, 3, 2, 1])) 
 
+## 3
+# def greater(num):
+#   check = []
+#   res = [-1] * len(num)
+#   for n in range(len(num)):
+#     check.append(num[n])
+#   for n in range(len(num)):
+#     if len(check) > 0:
+#       first = check[-1]
+#       if first > num[n]:
+#         res[n] = first
+#   return res
+
+# print(greater([4, 3, 4, 6, 5])) 
+# print(greater([6, 5, 4, 3, 2, 1])) 
+
+## 4
+# def greater(num):
+#   check = []
+#   res = [-1] * len(num)
+
+#   for n in range(len(num)):
+#     while len(check) > 0:
+#       last = check[-1]
+#       if num[n] > num[last]:
+#         check.pop()
+#         res[last] = num[n]
+#       else:
+#         break
+#     check.append(n)
+
+#   return res
+
+# print(greater([4, 3, 4, 6, 5])) 
+# print(greater([6, 5, 4, 3, 2, 1])) 
+
+
+## Q: Problem: Remove Adjacent Duplicates
+## 1
+# def remove(d):
+#   d = d.replace('nnn', '')
+#   d = d.replace('sss', '')
+#   return d
+
+# print(remove('annnaca'))
+# print(remove('azsszy'))
+
+## 2
+# def remove(s):
+#   res = ''
+#   for ch in s:
+#     if ch not in res:
+#       res = res + ch
+#   return res
+
+# print(remove('annnaca'))
+# print(remove('azsszy'))
+
+## 3
+# def remove(val):
+#   check = []
+#   for v in val:
+#     if len(check) == 0:
+#         check.append(v)
+#     else:
+#       first = check[-1]
+#       if first == v:
+#         check.pop()
+#       else:
+#         check.append(v)
+#   res = ''
+
+#   for v in check:
+#     res = res + v
+#   return res
+
+# print(remove('annnaca'))
+# print(remove('azsszy'))
+
+
+## Q: Problem: Min Stack check
+## 1
+class MinStack:
+  def __init__(self):
+    self.check = []
+  def push(self, v):
+    self.check.append(v)
+  def pop(self):
+    self.check.pop()
+  def Min(self):
+    return min(self.check)
+
+obj = MinStack()
+obj.push(6)
+obj.push(7)
+print(obj.Min())   
+
+obj.push(4)
+obj.push(3)
+print(obj.Min())   
+
+obj.pop()
+print(obj.Min())
+
+
+    
+
 
 
 
